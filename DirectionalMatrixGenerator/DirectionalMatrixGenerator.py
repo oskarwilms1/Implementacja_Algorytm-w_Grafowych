61a1f6fe7bc07370ff9738dffbb0129395f0517f
oskarwilms1 <149292696+oskarwilms1@users.noreply.github.com>
2024-10-11 16:02:43 +0200

    Update DirectionalMatrixGenerator.py

--- DirectionalMatrixGenerator/DirectionalMatrixGenerator.py
+++ DirectionalMatrixGenerator/DirectionalMatrixGenerator.py
@@ -13,16 +13,25 @@ Vertices = list(map(str,input().split()))
 Sequence = list(map(str,input().split()))
 Edges = []
 iteration = 0
-
-while iteration <= len(Vertices)-2:
+length = len(Vertices)
+while iteration < length:
     iteration2 = 0
-    while iteration2 <= len(Vertices)-1:
-        if str(int(Vertices[iteration2]) - int(Vertices[iteration])) in Vertices:
-            Edges.append(str(iteration2)+','+str(iteration))
+    while iteration2 < length:
+        if str(int(Vertices[iteration2]) - int(Vertices[iteration])) in Sequence:
+            Edges.append(str(iteration)+','+str(iteration2))
         iteration2 += 1
     iteration += 1
-print(Edges)
 
+matrix = [['0' for space in range(length)] for rows in range(length)]
+iteration = 0
+while iteration < len(Edges):
+    temp_list = Edges[iteration].split(',')
+    x = int(temp_list[0])
+    y = int(temp_list[1])
+    matrix[x][y] = '1'
+    iteration += 1
+for row in matrix:
+    print(' '.join(row))
 
 
 
