3bc394f1416805fd87023be0535425e74b7e4b4b
oskarwilms1 <oskar.wilms1@gmail.com>
2024-10-21 11:52:03 +0200

    make directories for files

--- MatrixFromAdjacencyListGenerator/MatrixFromAdjacencyListGenerator.py
+++ MatrixFromAdjacencyListGenerator/MatrixFromAdjacencyListGenerator.py
@@ -1,6 +1,7 @@
 iteration = 0
 iteration2 = 0
 matrix = []
+adjacency_list = []
 while True:
     try:
         temp_list = list(map(str,input().split()))
@@ -10,6 +11,15 @@ while True:
         break
     matrix.append(temp_list)
 while iteration < len(matrix):
+    temp_list = []
+    iteration2 = 0
     while iteration2 < len(matrix):
-        pass
-    
+        if matrix[iteration][iteration2] == '1':
+           temp_list.append(" "+str(iteration2+1))
+        iteration2 += 1 
+    adjacency_list.append(temp_list)
+    iteration +=1
+iteration = 0
+while iteration < len(matrix):
+    print(str(iteration+1)+''.join(adjacency_list[iteration]))
+    iteration +=1
