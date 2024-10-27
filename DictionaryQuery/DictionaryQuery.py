8cf9bcab814f16ce32d9183e2bd39c1d692d0101
oskarwilms1 <o.wilms.062@studms.ug.edu.pl>
2024-10-27 17:48:00 +0100

    revert last changes

--- DictionaryQuery/DictionaryQuery.py
+++ DictionaryQuery/DictionaryQuery.py
@@ -1,42 +1,25 @@
-try:
-    AdjacencyList = eval(input())
-except SyntaxError:
-    exit(1)
-
+AdjacencyList = eval(input())
 NumberOfQueries = int(input())
 ConnectionQueries = []
 NeighboursQueries = []
 
-for iteration in range(NumberOfQueries):
+for iteration in range(0,NumberOfQueries):
     try:
         user_input = input()
     except EOFError:
-        break
+        break 
     temp_list = user_input.split(' ')
     if temp_list[0] == 'neighbours':
         NeighboursQueries.append(temp_list[1])
     elif temp_list[0] == 'connection':
-        if len(temp_list) != 3:
-            continue
         ConnectionQueries.append(temp_list[1])
         ConnectionQueries.append(temp_list[2])
-
 for element in NeighboursQueries:
-    try:
-        node_index = int(element)
-        print(len(AdjacencyList.get(node_index, [])))
-    except ValueError:
-        exit(1)
-
+    print(len(AdjacencyList[int(element)]))
 iteration = 0
 while iteration < len(ConnectionQueries):
-    try:
-        node1 = int(ConnectionQueries[iteration])
-        node2 = int(ConnectionQueries[iteration + 1])
-        if node2 in AdjacencyList.get(node1, []):
-            print("Yes")
-        else:
-            print("No")
-    except (ValueError, IndexError):
-        exit(1)
-    iteration += 2
+    if int(ConnectionQueries[iteration]) in AdjacencyList[int(ConnectionQueries[iteration+1])]:
+        print("Yes")
+    else:
+        print("No")
+    iteration +=2
