bfaeb25d14683f7819a0b17b43d5af0ea68924e8
oskarwilms1 <o.wilms.062@studms.ug.edu.pl>
2024-10-27 19:19:44 +0100

    Add finished and tested CheckifGraphical.py

--- CheckIfGraphical/CheckIfGraphical.py
+++ CheckIfGraphical/CheckIfGraphical.py
@@ -5,17 +5,16 @@ while iteration < len(user_input):
     user_input[iteration]= int(user_input[iteration])
     iteration +=1
 iteration = 0
-while iteration < len(user_input): 
-    user_input.sort(reverse = True)
-    iteration2 = iteration
-    temp = int(user_input[iteration])
+length = len(user_input)
+while iteration < length: 
+    iteration2 = 0
+    temp = int(user_input[iteration2])
     user_input.pop(0)
-    if iteration == temp:
-        user_input[0] = user_input[0] - 1
-    while iteration2 < temp:
+    while iteration2 <= temp-1:
         user_input[iteration2] = user_input[iteration2] - 1
         iteration2 +=1
     if -1 in user_input:
         is_graphical = "NIE"
+        break
     iteration +=1
 print(is_graphical)
