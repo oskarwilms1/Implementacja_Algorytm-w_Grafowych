8b3c8ff41127a30a25dfb6de7638c44e5a6ecca1
oskarwilms1 <oskar.wilms1@gmail.com>
2024-11-05 12:03:38 +0100

    finished DictionaryQuery

--- DictionaryQuery/DictionaryQuery.py
+++ DictionaryQuery/DictionaryQuery.py
@@ -2,6 +2,9 @@ AdjacencyList = eval(input())
 NumberOfQueries = int(input())
 ConnectionQueries = []
 NeighboursQueries = []
+Queue = []
+
+iteration2 = 0
 
 for iteration in range(0,NumberOfQueries):
     try:
@@ -11,15 +14,20 @@ for iteration in range(0,NumberOfQueries):
     temp_list = user_input.split(' ')
     if temp_list[0] == 'neighbours':
         NeighboursQueries.append(temp_list[1])
+        Queue.append("N")
     elif temp_list[0] == 'connection':
         ConnectionQueries.append(temp_list[1])
         ConnectionQueries.append(temp_list[2])
-for element in NeighboursQueries:
-    print(len(AdjacencyList[int(element)]))
+        Queue.append("C")
 iteration = 0
-while iteration < len(ConnectionQueries):
-    if int(ConnectionQueries[iteration]) in AdjacencyList[int(ConnectionQueries[iteration+1])]:
-        print("Yes")
-    else:
-        print("No")
-    iteration +=2
+for element in Queue:
+    if element == "N":
+        print(len(AdjacencyList[int(NeighboursQueries[iteration])]))
+        iteration += 1
+    if element == "C":
+        if int(ConnectionQueries[iteration2]) in AdjacencyList[int(ConnectionQueries[iteration2+1])]:
+            print("Yes")
+        else:
+            print("No")
+        iteration2 += 2
+
